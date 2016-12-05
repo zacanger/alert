7ab9a5b9d8013908529b2739ef088a2e180cb1ed
zacanger <zac@zacanger.com>
2016-12-04 21:15:13 -0700

    the start of a thing

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// const { join } = require('path')
-// const { spawn } = require('child_process')
+let alert
+if (process.browser) {
+  alert = window.alert
+  process.exit(0)
+}
+
+/*
+let prog
+const { join } = require('path')
+const { spawn } = require('child_process')
+const { platform }= process
+
+switch (platform) {
+  case 'linux':
+  case 'darwin':
+  case 'freebsd':
+  case 'sunos':
+  case 'win32':
+}
+*/
 
 console.log('coming soon')
