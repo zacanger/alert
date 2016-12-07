8797b32f92f99b4d4fb779aac8995270d2e7dc4e
zacanger <zac@zacanger.com>
2016-12-06 20:10:28 -0700

    This is starting to become almost a thing.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-let alert
-if (process.browser) {
-  alert = window.alert
-  process.exit(0)
-}
-
-/*
-let prog
-const { join } = require('path')
 const { spawn } = require('child_process')
-const { platform }= process
+const { platform } = process
 
-switch (platform) {
-  case 'linux':
-  case 'darwin':
-  case 'freebsd':
-  case 'sunos':
-  case 'win32':
+const chooseAlert = () => {
+  let theAlert, theOS, theCmds
+  if (window && window.alert && typeof window.alert === 'function') {
+    theAlert = (str) => window.alert(str)
+    return theAlert
+  } else {
+    theAlert = (cmds) => spawn(cmds[0], cmds.splice(1))
+
+    switch (platform) {
+      case 'linux':
+      case 'freebsd':
+      case 'sunos':
+        theOS = 'nix'
+        break
+      case 'darwin':
+        theOS = 'mac'
+        break
+      case 'win32':
+        theOS = 'win'
+        break
+      default:
+        theOS = 'unknown'
+    }
+
+    if (theOS === 'nix') {
+      // if zenity
+      theCmds = (str) => [ 'zenity', '--notification', '--text', str ]
+    }
+    if (theOS === 'mac') {
+      // assuming applescript
+      theCmds = (str) => [ 'osascript', '-e', `tell app "System Events" to display dialog "${str}"` ]
+    }
+    if (theOS === 'win') {
+      // who the fuck knows
+    }
+
+    if (theOS === 'unknown') {
+      theAlert = (str) => console.log(str)
+    }
+    // return the alert that chooses the cmds
+  }
 }
-*/
 
-console.log('coming soon')
+module.exports = (input = '') => chooseAlert()(input)
