27696a1e195a5f1d646909b206c88607d9db8a3a
zacanger <zac@zacanger.com>
2017-01-08 10:56:42 -0700

    Most of a 'thing to use' thing.

--- README.md
+++ README.md
@@ -22,10 +22,29 @@ import alert from 'alert-node'
 alert('howdy')
 ```
 
+If using the API, you can specify a program to use. This can be one of:
+
+* zenity
+* yad
+* notify-send
+* xmessage
+* console
+* osascript
+* cscript
+* window
+
+Note that this will override any internal checks to get the correct program,
+and will blow up if you get it wrong. It's advised to just use the default behavior.
+
+```javascript
+import alert from 'alert-node'
+alert('hey!', 'yad')
+```
+
 `alert-node` also has a cli. `npm i -g alert-node` and run `alert 'sup brah'`.
 
 ## TODO:
 
-If `cscript` thing fails, fallback to `msg` thing.
+* If `cscript` thing fails, fallback to `msg` thing.
 
 License: WTFPL
--- index.js
+++ index.js
@@ -5,7 +5,42 @@ const { log } = console
 const getCmd = resolve(__dirname, 'get-cmd.sh')
 const windowsScript = join(__dirname, 'msgbox.vbs')
 
-const makeAlert = (input = '') => {
+const makeAlert = (input = '', thingToUse) => {
+  if (thingToUse) {
+    if (thingToUse === 'window') {
+      return (str) => window.alert(str)
+    }
+    if (thingToUse === 'console') {
+      return (str) => log(str)
+    } else {
+      const theAlert = (cmds) => spawn(cmds[0], cmds.splice(1))
+      let theCmds = (str) => [ str ]
+      switch (thingToUse) {
+        case 'zenity':
+          theCmds = (str) => [ 'zenity', '--info', '--text', str ]
+          break
+        case 'yad':
+          theCmds = (str) => [ 'yad', '--text', str, '--button', 'OK' ]
+          break
+        case 'notify-send':
+          theCmds = (str) => [ 'notify-send', str ]
+          break
+        case 'xmessage':
+          theCmds = (str) => [ 'xmessage', str ]
+          break
+        case 'osascript':
+          theCmds = (str) => [ 'osascript', '-e', `tell app "System Events" to display dialog "${str}" buttons "OK"` ]
+          break
+        case 'cscript':
+          theCmds = (str) => [ 'cscript',  windowsScript, str ]
+          break
+        default:
+          return (str) => log(str)
+      }
+      return (str) => theAlert(theCmds(str))
+    }
+  }
+
   if (process.browser && window && window.alert && typeof window.alert === 'function') {
     return (str) => window.alert(str)
   } else {
