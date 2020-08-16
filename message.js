67a46d96ce34e5d676d33314e14418722beae515
zacanger <zac@zacanger.com>
2020-08-16 09:37:15 -0600

    chore: postinstall message as alert-node

---
+++ message.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const red = '\u001b[0;31m'
+const reset = '\u001b[0m'
+
+console.log()
+console.log()
+console.log()
+console.log()
+console.log(`${red}The package name alert-node has been deprecated.`)
+console.log(`${red}Please npm i alert instead${reset}.`)
+console.log()
+console.log()
+console.log()
+console.log()
